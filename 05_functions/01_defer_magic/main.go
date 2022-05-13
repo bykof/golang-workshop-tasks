2ba53ab244569c1b39086be51765a00d0179f58b
Michael Bykovski <bykof@me.com>
2022-05-13 08:37:38 +0200

    Add defer Hint

--- 05_functions/01_defer_magic/main.go
+++ 05_functions/01_defer_magic/main.go
@@ -1,10 +1,14 @@
 package main
 
 /*
-   Task:
-       Read a string from stdin.
-       Implement a function called PrintReverse(s string),
-	   which prints a string in the reverse order just by using defer!
+    Task:
+        Read a string from stdin.
+        Implement a function called PrintReverse(s string),
+        which prints a string in the reverse order just by using defer!
+
+
+        Hint:
+            Defer is STACKABLE therefore try to print every letter within a defer function
 
    Output:
        > Hello
